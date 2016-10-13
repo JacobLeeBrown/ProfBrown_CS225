ec0cf7d251c9e576dc6463c9592874f9190ce97f
Jacob Brown <jlbgreenbay@gmail.com>
2016-10-13 01:00:20 -0500

    Finished MP4.2

--- mp4/filler.cpp
+++ mp4/filler.cpp
@@ -11,244 +11,154 @@
 using std::vector;
 
 animation filler::dfs::fillSolid(PNG& img, int x, int y, RGBAPixel fillColor,
-                                 int tolerance, int frameFreq)
+								int tolerance, int frameFreq)
 {
-    /**
-     * @todo Your code here! You should replace the following line with a
-     * correct call to fill with the correct colorPicker parameter.
-     */
 	solidColorPicker solid(fillColor);	//Create a solid color
-    	//Pass all the work to the helper function
+	//Pass all the work to the helper function
 	return filler::fill<Stack>(img, x, y, solid, tolerance, frameFreq);
 }
 
 animation filler::dfs::fillGrid(PNG& img, int x, int y, RGBAPixel gridColor,
-                                int gridSpacing, int tolerance, int frameFreq)
+								int gridSpacing, int tolerance, int frameFreq)
 {
-    /**
-     * @todo Your code here! You should replace the following line with a
-     * correct call to fill with the correct colorPicker parameter.
-     */
-    	gridColorPicker grid(gridColor, gridSpacing);
+	gridColorPicker grid(gridColor, gridSpacing);
 	//Pass all the work to the helper function
 	return filler::fill<Stack>(img, x, y, grid, tolerance, frameFreq);
 }
 
 animation filler::dfs::fillGradient(PNG& img, int x, int y,
-                                    RGBAPixel fadeColor1, RGBAPixel fadeColor2,
-                                    int radius, int tolerance, int frameFreq)
+								RGBAPixel fadeColor1, RGBAPixel fadeColor2,
+								int radius, int tolerance, int frameFreq)
 {
-    /**
-     * @todo Your code here! You should replace the following line with a
-     * correct call to fill with the correct colorPicker parameter.
-     */
 	gradientColorPicker gradient(fadeColor1, fadeColor2, radius, x, y);
-    	//Pass all the work to the helper function
+	//Pass all the work to the helper function
 	return filler::fill<Stack>(img, x, y, gradient, tolerance, frameFreq);	
 }
 
 animation filler::dfs::fill(PNG& img, int x, int y, colorPicker& fillColor,
-                            int tolerance, int frameFreq)
+							int tolerance, int frameFreq)
 {
-    /**
-     * @todo Your code here! You should replace the following line with a
-     * correct call to filler::fill with the correct template parameter
-     * indicating the ordering structure to be used in the fill.
-     */
 	//If I understand correctly, just pass everything sent in to the helper function
-    	return filler::fill<Stack>(img, x, y, fillColor, tolerance, frameFreq);
+	return filler::fill<Stack>(img, x, y, fillColor, tolerance, frameFreq);
 }
 
 animation filler::bfs::fillSolid(PNG& img, int x, int y, RGBAPixel fillColor,
-                                 int tolerance, int frameFreq)
+								int tolerance, int frameFreq)
 {
-    /**
-     * @todo Your code here! You should replace the following line with a
-     * correct call to fill with the correct colorPicker parameter.
-     */
 	solidColorPicker solid(fillColor);
 	//Pass all the work to the helper function. Same as DFS but uses a 
 	// Queue instead of a Stack
-    	return filler::fill<Queue>(img, x, y, solid, tolerance, frameFreq);
+	return filler::fill<Queue>(img, x, y, solid, tolerance, frameFreq);
 }
 
 animation filler::bfs::fillGrid(PNG& img, int x, int y, RGBAPixel gridColor,
                                 int gridSpacing, int tolerance, int frameFreq)
 {
-    /**
-     * @todo Your code here! You should replace the following line with a
-     * correct call to fill with the correct colorPicker parameter.
-     */
 	gridColorPicker grid(gridColor, gridSpacing);
-    	//Pass all the work to the helper function
+	//Pass all the work to the helper function
 	return filler::fill<Queue>(img, x, y, grid, tolerance, frameFreq);
 }
 
 animation filler::bfs::fillGradient(PNG& img, int x, int y,
-                                    RGBAPixel fadeColor1, RGBAPixel fadeColor2,
-                                    int radius, int tolerance, int frameFreq)
+									RGBAPixel fadeColor1, RGBAPixel fadeColor2,
+									int radius, int tolerance, int frameFreq)
 {
-    /**
-     * @todo Your code here! You should replace the following line with a
-     * correct call to fill with the correct colorPicker parameter.
-     */
 	gradientColorPicker gradient(fadeColor1, fadeColor2, radius, x, y);
 	//Pass all the work to the helper function
-    	return filler::fill<Queue>(img, x, y, gradient, tolerance, frameFreq);
+	return filler::fill<Queue>(img, x, y, gradient, tolerance, frameFreq);
 }
 
 animation filler::bfs::fill(PNG& img, int x, int y, colorPicker& fillColor,
-                            int tolerance, int frameFreq)
+							int tolerance, int frameFreq)
 {
-    /**
-     * @todo Your code here! You should replace the following line with a
-     * correct call to filler::fill with the correct template parameter
-     * indicating the ordering structure to be used in the fill.
-     */
 	//Same as DFS but uses a Queue instead of a Stack
-    	return filler::fill<Queue>(img, x, y, fillColor, tolerance, frameFreq);
+	return filler::fill<Queue>(img, x, y, fillColor, tolerance, frameFreq);
 }
 
 template <template <class T> class OrderingStructure>
 animation filler::fill(PNG& img, int x, int y, colorPicker& fillColor,
-                       int tolerance, int frameFreq)
-{
-    /**
-     * @todo You need to implement this function!
-     *
-     * This is the basic description of a flood-fill algorithm: Every fill
-     * algorithm requires an ordering structure, which is passed to this
-     * function via its template parameter. For a breadth-first-search
-     * fill, that structure is a Queue, for a depth-first-search, that
-     * structure is a Stack. To begin the algorithm, you simply place the
-     * given point in the ordering structure. Then, until the structure is
-     * empty, you do the following:
-     *
-     * 1.     Remove a point from the ordering structure.
-     *
-     *        If it has not been processed before and if its color is
-     *        within the tolerance distance (up to and **including**
-     *        tolerance away in square-RGB-space-distance) to the original
-     *        point's pixel color [that is, \f$(currentRed - OriginalRed)^2 +
-              (currentGreen - OriginalGreen)^2 + (currentBlue -
-              OriginalBlue)^2 \leq tolerance\f$], then:
-     *        1.    indicate somehow that it has been processed (do not mark it
-     *              "processed" anywhere else in your code)
-     *        2.    change its color in the image using the appropriate
-     *              colorPicker
-     *        3.    add all of its neighbors to the ordering structure, and
-     *        4.    if it is the appropriate frame, send the current PNG to the
-     *              animation (as described below).
-     * 2.     When implementing your breadth-first-search and
-     *        depth-first-search fills, you will need to explore neighboring
-     *        pixels in some order.
-     *
-     *        While the order in which you examine neighbors does not matter
-     *        for a proper fill, you must use the same order as we do for
-     *        your animations to come out like ours! The order you should put
-     *        neighboring pixels **ONTO** the queue or stack is as follows:
-     *        **RIGHT(+x), DOWN(+y), LEFT(-x), UP(-y). IMPORTANT NOTE: *UP*
-     *        here means towards the top of the image, so since an image has
-     *        smaller y coordinates at the top, this is in the *negative y*
-     *        direction. Similarly, *DOWN* means in the *positive y*
-     *        direction.** To reiterate, when you are exploring (filling out)
-     *        from a given pixel, you must first try to fill the pixel to
-     *        it's RIGHT, then the one DOWN from it, then to the LEFT and
-     *        finally UP. If you do them in a different order, your fill may
-     *        still work correctly, but your animations will be different
-     *        from the grading scripts!
-     * 3.     For every k pixels filled, **starting at the kth pixel**, you
-     *        must add a frame to the animation, where k = frameFreq.
-     *
-     *        For example, if frameFreq is 4, then after the 4th pixel has
-     *        been filled you should add a frame to the animation, then again
-     *        after the 8th pixel, etc.  You must only add frames for the
-     *        number of pixels that have been filled, not the number that
-     *        have been checked. So if frameFreq is set to 1, a pixel should
-     *        be filled every frame.
-     */
-	
-    // The ordering stucture that will hold the elements as they are processed
+						int tolerance, int frameFreq)
+{	
+	// The ordering stucture that will hold the elements as they are processed
 	OrderingStructure<RGBAPixel*> dickPix;
-    // The same ordering structures that keep track of x and y coords of corresponding points
-    OrderingStructure<int> dickPixX;
-    OrderingStructure<int> dickPixY;
-    // The animation that will show the algorithm
+	// The same ordering structures that keep track of x and y coords of corresponding points
+	OrderingStructure<int> dickPixX;
+	OrderingStructure<int> dickPixY;
+	// The animation that will show the algorithm
 	animation anim;
 
-    // The 2D bool array that will keep track of which pixels are processed
-    // Initialize every entry to false since we've yet to check any pixels
-    int height = img.height();
-    int width = img.width();
+	// The 2D bool array (implemented with vectors) that will keep track of which pixels are processed
+	// Initialize every entry to false since we've yet to check any pixels
+	int height = img.height();
+	int width = img.width();
 	vector<bool> rows(width, false);
-    vector<vector<bool>> processed(height, rows);
-    /*bool processed[height][width];
-    for(int yi = 0; yi < height; yi++)
-    {
-        for(int xi = 0; xi < width; xi++)
-        {
-            processed[yi][xi] = false;
-        }
-    }*/
+	vector<vector<bool>> processed(height, rows);
 
-    // The first point checked, add it to the OrderingStructure
-	RGBAPixel* first = img(x, y);
-	dickPix.add(first);
-    // Counter for how many pixels have been modified
-	int modPix = 0;
-    while(!dickPix.isEmpty())
+	// Add the first point and its coordinates to the OrderingStructures
+	dickPix.add(img(x, y));
+	dickPixX.add(x);
+	dickPixY.add(y);
+	// Save the RGB values of the first point for tolerance reference
+	int fRed = img(x, y)->red;
+	int fGreen = img(x, y)->green;
+	int fBlue = img(x, y)->blue;
+
+	// Counter for how many pixels have been filled
+	int filledPix = 0;
+	while(!(dickPix.isEmpty()))
 	{
-        // obtain current pixel and coordinates
+		// obtain current pixel and coordinates
 		RGBAPixel* cur = dickPix.remove();
-        int curX = dickPixX.remove();
-        int curY = dickPixY.remove();
+		int curX = dickPixX.remove();
+		int curY = dickPixY.remove();
 
-		int euclidian = (int)pow((first->red - cur->red), 2) +
-				(int)pow((first->green - cur->green), 2) +
-				(int)pow((first->blue - cur->blue), 2);
+		int euclidian = (int)pow((cur->red - fRed), 2) +
+						(int)pow((cur->green - fGreen), 2) +
+						(int)pow((cur->blue - fBlue), 2);
+
 		if(euclidian <= tolerance && !processed[curY][curX])
 		{
 			img(curX, curY)->red = fillColor(curX, curY).red;
-            img(curX, curY)->green = fillColor(curX, curY).green;
-            img(curX, curY)->blue = fillColor(curX, curY).blue;
-			modPix++;
-			if(modPix == frameFreq)
+			img(curX, curY)->green = fillColor(curX, curY).green;
+			img(curX, curY)->blue = fillColor(curX, curY).blue;
+
+			filledPix++;
+			if(filledPix == frameFreq)
 			{
 				anim.addFrame(img);
-				modPix = 0;
+				filledPix = 0;
 			}
-            processed[curY][curX] = true;
+
+			// add the right neighbor and its respective coordinates if valid
+			if(curX+1 < width)
+			{
+				dickPix.add(img(curX+1, curY));
+				dickPixX.add(curX+1);
+				dickPixY.add(curY);
+			}
+			// add the down neighbor and its respective coordinates if valid
+			if(curY+1 < height)
+			{
+				dickPix.add(img(curX, curY+1));
+				dickPixX.add(curX);
+				dickPixY.add(curY+1);
+			}
+			// add the left neighbor and its respective coordinates if valid
+			if(curX-1 >= 0)
+			{
+				dickPix.add(img(curX-1, curY));
+				dickPixX.add(curX-1);
+				dickPixY.add(curY);
+			}
+			// add the up neighbor and its respective coordinates if valid
+			if(curY-1 >= 0)
+			{
+				dickPix.add(img(curX, curY-1));
+				dickPixX.add(curX);
+				dickPixY.add(curY-1);
+			}
 		}
-        
-        // add the right neighbor and its respective coordinates if valid
-        if(curX+1 < width)
-        {
-    		dickPix.add(img(curX+1, curY));
-            dickPixX.add(curX+1);
-            dickPixY.add(curY);
-        }
-        // add the down neighbor and its respective coordinates if valid
-        if(curY+1 < height)
-        {
-            dickPix.add(img(curX, curY+1));
-            dickPixX.add(curX);
-            dickPixY.add(curY+1);
-        }
-        // add the left neighbor and its respective coordinates if valid
-        if(curX-1 > 0)
-        {
-            dickPix.add(img(curX-1, curY));
-            dickPixX.add(curX-1);
-            dickPixY.add(curY);
-        }
-        // add the up neighbor and its respective coordinates if valid
-        if(curY-1 > 0)
-        {
-            dickPix.add(img(curX, curY-1));
-            dickPixX.add(curX);
-            dickPixY.add(curY-1);
-        }
+		processed[curY][curX] = true;
 	}
-	
-    return anim;
+	return anim;
 }
